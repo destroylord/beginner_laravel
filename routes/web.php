09a1f21fd9b116d33ef995b75d6e87046757746e
destroylord <masapin68@gmail.com>
2020-07-12 17:56:26 +0700

    belajar routing

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,15 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $name = "Fahmi Dafrin Maulana";
+
+    return view('welcome', ['name' => $name]);
 });
+
+// Route::view('/', 'welcome');
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::view('series/create', 'series.premium.show');
