e7203dc73d32d0da1308e21a84b02b47f9868179
destroylord <masapin68@gmail.com>
2020-07-13 16:18:13 +0700

    eloquent mantap

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Post;
 
 class PostController extends Controller
 {
@@ -11,8 +11,12 @@ class PostController extends Controller
     public function show($slug)
     {
 
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-        // dd($post);
+        $post = Post::where('slug', $slug)->first();
+
+        if (!$post) {
+            abort(404);
+        }
+
         return view('posts.show', compact('post'));
     }
 }
