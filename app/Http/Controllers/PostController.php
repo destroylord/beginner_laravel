7529d2174e649d9a57282711859a5f9103541bb2
destroylord <masapin68@gmail.com>
2020-07-13 15:59:35 +0700

    meet database

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class PostController extends Controller
 {
     //
     public function show($slug)
     {
-        return view('posts.show', compact('slug'));
+
+        $post = \DB::table('posts')->where('slug', $slug)->first();
+        // dd($post);
+        return view('posts.show', compact('post'));
     }
 }
