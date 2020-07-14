5ca21366e6d62e57863e7f50047f60584831082f
destroylord <masapin68@gmail.com>
2020-07-14 07:36:22 +0700

    eloquent menggunakan tinker

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     //
+    // menampilkan data terahkirt
+    public function scopeLatestFirst()
+    {
+        return $this->latest()->first();
+    }
+
+    public function scopeLatestPost()
+    {
+        return $this->latest()->get();
+    }
 }
